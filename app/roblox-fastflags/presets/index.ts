d0305d7f302db4d0ddcd5b26fa1eb8ded9755f30
WAYANABRAHAM <wayan.abraham@gmail.com>
2025-06-16 08:12:28 +0700

    Remove preset1, preset2, and preset3 and update index.ts

--- app/roblox-fastflags/presets/index.ts
+++ app/roblox-fastflags/presets/index.ts
@@ -1,7 +1,5 @@
 import { PresetFile } from "../types";
-import { preset1 } from "./preset1";
-import { preset2 } from "./preset2";
-import { preset3 } from "./preset3";
+
 import { preset4 } from "./preset4";
 import { preset5 } from "./preset5";
 import { preset6 } from "./preset6";
@@ -11,4 +9,4 @@ import { preset9 } from "./preset9";
 import { preset10} from "./preset10";
 
 
-export const RECOMMENDED_PRESETS: PresetFile[] = [preset1, preset2, preset3, preset4, preset5, preset6, preset7, preset8, preset9, preset10];
+export const RECOMMENDED_PRESETS: PresetFile[] = [preset4, preset5, preset6, preset7, preset8, preset9, preset10];
